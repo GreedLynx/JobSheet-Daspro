45ffc0db166567ea51afab8e1c85df69af845e3b
nopalppt <naufalshofil1603@gmail.com>
2025-10-06 21:06:13 +0700

    Modifikasi Percobaan 1

---
+++ JobSheet_5/Readme.md
@@ -0,0 +1,15 @@
+# jawaban untuk pertanyaan Jobsheet 5
+
+## Pertanyaan dan jawaban
+
+- 1. Mengapa pengecekan pada struktur IF tersebut tidak melibatkan kondisi dengan operator relasional?
+
+  - karena variabel uktLunas sudah merupakan nilai Boolean (True/False).
+
+- 2. Saat program dijalankan, kemudian Anda mengisikan nilai false, bagaimana hasilnya?
+
+  - tidak menghasilkan apa apa, karena tidak memenuhi kondisi apapun.
+
+- 3. Sistem perlu memberikan informasi apabila pengguna memasukkan nilai false, maka terdapat keluar “Registrasi ditolak. Silakan lunasi UKT terlebih dahulu”. Modifikasi program tersebut dengan menambahkan struktur ELSE!
+- 4. Commit dan push hasil modifikasi Anda ke Github dengan pesan “Modifikasi
+     Percobaan 1”
---
+++ JobSheet_5/ifCetakKRS20.java
@@ -0,0 +1,21 @@
+package JobSheet_5;
+
+import java.util.Scanner; //deklarasi agar java bisa nerima input
+
+public class ifCetakKRS20 {
+    public static void main(String[] args) {
+        try(Scanner input = new Scanner(System.in)){ //makai try pengganti sc.close
+            System.out.println("---Cetak KRS SIAKAD---");
+            System.out.print("Apakah UKT sudah lunas? (True/False) : ");
+            boolean uktLunas = input.nextBoolean();
+
+            if(uktLunas){ //Deklarasi pemenuhan kondisi
+                System.out.println("Pembayaran UKT Terverifikasi");
+                System.out.println("Silahkan cetak KRS dan minta tanda tangan ke DPA!");
+            }
+            else {
+                System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu!");
+            }
+        }
+    }
+}
